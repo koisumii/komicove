edc703f154bb0a2e0137b4d7a91ebbc4a559cf4e
koisumii <2256255@johnabbottcollege.net>
2024-12-16 22:01:57 -0500

    Complete fish factory with all fish types and images integrated

--- src/enums/ImageName.js
+++ src/enums/ImageName.js
@@ -1,7 +1,55 @@
 const ImageName = {
     Background: 'background',
     Tiles: 'tiles',
-    FishBitterling: 'bitterling'
+    // Common Fish
+    FishAnchovy: 'anchovy',
+    FishBarbel: 'barbel',
+    FishBitterling: 'bitterling',
+    FishCarp: 'carp',
+    FishCatfish: 'catfish',
+    FishCrucianCarp: 'crucian_carp',
+    FishFlounder: 'flounder',
+    FishGoldfish: 'goldfish',
+    FishKillfish: 'killfish',
+    FishLoach: 'loach',
+    FishMinnow: 'minnow',
+    FishRicefish: 'ricefish',
+    FishSmallmouthBass: 'smallmouth_bass',
+    FishSmelt: 'smelt',
+    FishStickleback: 'stickleback',
+    FishStrupedShiner: 'struped_shiner',
+    FishSweetfish: 'sweetfish',
+    FishTadpole: 'tadpole',
+    FishTuna: 'tuna',
+
+    // Rare Fish
+    FishArapaima: 'arapaima',
+    FishClownish: 'clownfish',
+    FishDorado: 'dorado',
+    FishGiantTrevallry: 'giant_trevally',
+    FishGoby: 'goby',
+    FishGuppy: 'guppy',
+    FishKoi: 'koi',
+    FishMoonfish: 'moonfish',
+    FishMorayEel: 'moray_eel',
+    FishNeotropicalSilverside: 'neotropical_silverside',
+    FishPaleChub: 'pale_chub',
+    FishTilapia: 'tilapia',
+    FishWalleye: 'walleye',
+
+    // Legendary Fish
+    FishAngelFish: 'angel_fish',
+    FishArowana: 'arowana',
+    FishBetta: 'betta',
+    FishBlueMarlin: 'blue_marlin',
+    FishCoelacanth: 'coelacanth',
+    FishJapaneseLates: 'japanese_lates',
+    FishKingCrab: 'king_crab',
+    FishLowlandCichlid: 'lowland_cichlid',
+    FishMahiMahi: 'mahi-mahi',
+    FishRainbowfish: 'rainbowfish',
+    FishSturgeon: 'sturgeon',
+    FishWhiteSpottedChar: 'white_spotted_char'
 };
 
 export default ImageName;
--- src/services/FishFactory.js
+++ src/services/FishFactory.js
@@ -1,8 +1,79 @@
 import CommonFish from "../entities/CommonFish.js";
+import LegendaryFish from "../entities/LegendaryFish.js";
+import RareFish from "../entities/RareFish.js";
 import ImageName from "../enums/ImageName.js";
 
 export default class FishFactory {
+    static COMMON_FISH_IMAGES = [
+        ImageName.FishAnchovy,
+        ImageName.FishBarbel,
+        ImageName.FishBitterling,
+        ImageName.FishCarp,
+        ImageName.FishCatfish,
+        ImageName.FishCrucianCarp,
+        ImageName.FishFlounder,
+        ImageName.FishGoldfish,
+        ImageName.FishKillfish,
+        ImageName.FishLoach,
+        ImageName.FishMinnow,
+        ImageName.FishRicefish,
+        ImageName.FishSmallmouthBass,
+        ImageName.FishSmelt,
+        ImageName.FishStickleback,
+        ImageName.FishStrupedShiner,
+        ImageName.FishSweetfish,
+        ImageName.FishTadpole,
+        ImageName.FishTuna
+    ];
+
+    static RARE_FISH_IMAGES = [
+        ImageName.FishArapaima,
+        ImageName.FishClownish,
+        ImageName.FishDorado,
+        ImageName.FishGiantTrevallry,
+        ImageName.FishGoby,
+        ImageName.FishGuppy,
+        ImageName.FishKoi,
+        ImageName.FishMoonfish,
+        ImageName.FishMorayEel,
+        ImageName.FishNeotropicalSilverside,
+        ImageName.FishPaleChub,
+        ImageName.FishTilapia,
+        ImageName.FishWalleye
+    ]
+
+    static LEGENDARY_FISH_IMAGES = [
+        ImageName.FishAngelFish,
+        ImageName.FishArowana,
+        ImageName.FishBetta,
+        ImageName.FishBlueMarlin,
+        ImageName.FishCoelacanth,
+        ImageName.FishJapaneseLates,
+        ImageName.FishKingCrab,
+        ImageName.FishLowlandCichlid,
+        ImageName.FishMahiMahi,
+        ImageName.FishRainbowfish,
+        ImageName.FishSturgeon,
+        ImageName.FishWhiteSpottedChar
+    ]
+
     static createFish() {
-        return new CommonFish('Bitterling', 5, ImageName.FishBitterling)
+        const random = Math.random();
+
+        if(random < 0.6) {
+            const image = this.randomImage(this.COMMON_FISH_IMAGES);
+            return new CommonFish('Common Fish', 5, image);
+        } else if (random < 0.9) {
+            const image = this.randomImage(this.RARE_FISH_IMAGES);
+            return new RareFish('Rare Fish', 10, image);
+        } else {
+            const image = this.randomImage(this.LEGENDARY_FISH_IMAGES);
+            return new LegendaryFish('Legendary Fish', 25, image);
+        }
+    }
+
+    static randomImage(imageArray){
+        const randomIndex = Math.floor(Math.random() * imageArray.length);
+        return imageArray[randomIndex];
     }
 }
